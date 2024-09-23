b98228fb8e2a478fd002b6bab219ae744e066a9d
Raja Sachin <sachinra@vmware.com>
2024-09-23 22:52:28 +0530

    airflow codes

---
+++ dags/user_processing.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+
+import json
+from datetime import datetime
+
+with DAG('user_processing', start_date=datetime(2024, 1, 1), schedule_interval='@daily', catchup=False) as dag:
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        '''
+    )
+
+    is_api_available = HttpSensor (
+        task_id = 'is_api_available',
+        http_conn_id = 'user_api',
+        endpoint = 'api/'
+    )
+
+    extract_user = SimpleHttpOperator(
+        task_id = 'extract_user',
+        http_conn_id = 'user_api',
+        endpoint = 'api/',
+        method = 'GET',
+        response_filter = lambda response: json.loads(response.text),
+        log_response = True
+    )
